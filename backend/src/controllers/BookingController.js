483c248cf1f479e1c91c39ecc994491cf4a6ab33
Alisson Matos <alissonmgsantos@gmail.com>
2019-10-01 13:09:05 -0300

    Cadastrando reservas e relacionamento com mongodb

---
+++ backend/src/controllers/BookingController.js
@@ -0,0 +1,25 @@
+const Booking = require('../models/Booking');
+
+const store = async (req, res) => {
+    const { user_id } = req.headers;
+    const { spot_id } = req.params;
+    const { date } = req.body;
+
+    const booking = await Booking.create({
+        user: user_id,
+        spot: spot_id,
+        date,        
+    });
+
+    await booking.populate('spot').populate('user').execPopulate();
+
+    return res.json(booking);
+}
+
+module.exports = {
+// index,
+// show,
+store,
+// update,
+// destroy
+}
