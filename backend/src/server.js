869f571c2e2d2fbfe95aa01c1e8cc592827503ac
Alisson Matos <alissonmgsantos@gmail.com>
2019-10-01 11:11:10 -0300

    Registrando usuário na base de dados

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 const app = express();
 
-// GET, POST, PUT, DELETE
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/OmniStack', { useNewUrlParser: true, useUnifiedTopology: true });
 
+// GET, POST, PUT, DELETE
 // req.query = Acessar query params (para filtros)
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (criação e edição)
