ce740f783197c4205b92fc6efd2b1706c3865237
Alisson Matos <alissonmgsantos@gmail.com>
2019-10-02 11:30:54 -0300

    Ajuste de cors, criação de interface com react

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 const app = express();
@@ -11,8 +13,9 @@ mongoose.connect('mongodb://localhost:27017/OmniStack', { useNewUrlParser: true,
 // req.query = Acessar query params (para filtros)
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (criação e edição)
-
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
